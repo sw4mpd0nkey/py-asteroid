51956e90bb84e8c52eb52580a28dca4ce3edac65
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-04-06 12:23:33 -0400

    made changes to use less resources and start tracking game loop time

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ def main():
     #print(f"Screen width: {constants.SCREEN_WIDTH}")
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # game loop
     while True:
         log_state()
 
         # process the pygame event queue
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.quit:
+                return
 
         color = (0, 0, 0)
         screen.fill(color)
         pygame.display.flip()
+
+        # pause game loop for 1/60th sec
+        dt = (clock.tick(60) / 1000)
+        print(dt)
 
 if __name__ == "__main__":
     main()

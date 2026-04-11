53fac7325205d52b49db75be5a390c1014bdc6c7
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-04-11 14:18:16 -0400

    added up down movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_RADIUS, TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -31,3 +31,13 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             # preseeing d to turn right
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
